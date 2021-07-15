3cab34f6c15a1f0980068dc24c451ed71ad6de87
Gönül Çetin <71704035+gnlctn@users.noreply.github.com>
2021-07-15 12:54:08 +0300

    Update week3projectreverse.py

--- week3projectreverse.py
+++ week3projectreverse.py
@@ -1,20 +1,20 @@
 #Verilen listenin içindeki elemanları tersine döndüren bir fonksiyon yazın.
 # Eğer listenin içindeki elemanlar da liste içeriyorsa onların elemanlarını da tersine döndürün
-def Reverse(xlist, flist):
+def Reverse(rlist, reversedlist):
     
-    xlist.reverse()
+    rlist.reverse()
    # print(xlist)
-    for i in xlist:
+    for i in rlist:
         if isinstance(i, list):
-            Reverse(i, flist)
+            Reverse(i, reversedlist)
         else:
             """a=xlist.index(i)  
             print(a)   index kullanarak listeyi yeni bir listeye aktarmadan reverse yapabilir miyim
             onu denemeye çalıştım"""
             
-            flist.append(xlist)
+            reversedlist.append(rlist)
             break
-    return flist
+    return reversedlist
 print(Reverse([[1, 2], [3, 4], [5, 6, 7]], []))
 #[1,[1, 2], [3, 4], [5, 6, [7]]] gibi derin yapıda doğru sonuç görünmüyor.
 #Düzenlemeye çalıştım fakat sağlıklı yapı kuramadım.
