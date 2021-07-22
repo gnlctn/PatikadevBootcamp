a38e0afff3c3b78821407a9f21f40f89fe3a4f14
Gönül Çetin <71704035+gnlctn@users.noreply.github.com>
2021-07-22 15:08:29 +0300

    Update SQLodev11.sql

--- SQLodev11.sql
+++ SQLodev11.sql
@@ -4,3 +4,19 @@
 union all
 ( select first_name from customer
 )
+
+
+--actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+( select first_name from actor
+)
+intersect
+( select first_name from customer
+)
+
+
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+( select first_name from actor
+)
+except
+( select first_name from customer
+)
