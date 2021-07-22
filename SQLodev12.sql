f6ec9af7cb74eeda8ed258767983372253c26b62
Gönül Çetin <71704035+gnlctn@users.noreply.github.com>
2021-07-22 15:47:40 +0300

    Create SQLodev12.sql

---
+++ SQLodev12.sql
@@ -0,0 +1,3 @@
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+select count(*) as filmsayisi from film
+where length >(select round(avg(length)) from film )
